ed9647fd564872cf0dace84f8dc2c7627410201b
Lương Tân <luongnhattan2807@gmail.com>
2021-12-13 07:16:17 +0000

    Finish Introduction and Django Admin

--- blango/settings.py
+++ blango/settings.py
@@ -47,12 +47,17 @@ class Dev(Configuration):
 			'django.contrib.contenttypes',
 			'django.contrib.sessions',
 			'django.contrib.messages',
+			'django.contrib.sites',
 			'django.contrib.staticfiles',
 			'blango_auth',
 			'blog',
 			'crispy_forms',
 			'crispy_bootstrap5',
-			'debug_toolbar'
+			'debug_toolbar',
+			'allauth',
+			'allauth.account',
+			'allauth.socialaccount',
+			'allauth.socialaccount.providers.google',
 	]
 
 	MIDDLEWARE = [
@@ -209,6 +214,13 @@ class Dev(Configuration):
 	#email
 	EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 	ACCOUNT_ACTIVATION_DAYS = 7
+	
+	# Site
+	SITE_ID = 1
+	ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+	ACCOUNT_EMAIL_REQUIRED = True
+	ACCOUNT_USERNAME_REQUIRED = False
+	ACCOUNT_AUTHENTICATION_METHOD = "email"
 	
 	
 class Prod(Dev):
