f99245ba52e51514a84c401c4b9b27088e0193f6
Lương Tân <luongnhattan2807@gmail.com>
2021-12-20 15:23:59 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,1 +1,18 @@
-alert('Hello, world!')
+function sayHello(name) {
+  if (name === undefined) {
+      console.log('Hello, no name')
+  } else {
+       console.log('Hello, ' + name)
+  }
+}
+
+const name = 'Your Name'  // Put your name here
+
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(name)
+  }, 2000
+)
+
+console.log('After setTimeout')
